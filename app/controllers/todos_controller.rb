30c5037dd96ce63e31c2b1e1dc7335388b57b382
MaHa63 <matti.harsu@gmail.com>
2020-03-17 10:29:49 -0400

    Some notification related changes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -105,7 +105,7 @@ class TodosController < ApplicationController
   def destroy
     @todo.destroy
     respond_to do |format|
-      format.html { redirect_to todos_url, notice: 'Todo was successfully destroyed.' }
+      format.html { redirect_to todos_url, notice: 'Tehtävä poistettu.' }
       format.json { head :no_content }
     end
   end
